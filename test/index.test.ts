27474f0ce5719eb434331210d5d19e1b61988378
kizahasi <rasis.aneki@gmail.com>
2021-07-20 02:25:07 +0900

    improve test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,20 @@
 import { Result } from '../src';
 
-describe('index', () => {
-    it('works', () => {
-        expect(Result.ok(1)).toEqual({ isError: false, value: 1 });
-    });
+it('tests Ok', () => {
+    const okObj = Result.ok(1);
+    if (okObj.isError) {
+        fail('should not be error');
+    } else {
+        expect(okObj.value).toBe(1);
+    }
+});
+
+it('tests Error', () => {
+    const errorValue = 'ERROR';
+    const errorObj = Result.error(errorValue);
+    if (errorObj.isError) {
+        expect(errorObj.error).toBe(errorValue);
+    } else {
+        fail('should be error');
+    }
 });
