24296e69720fdb8a12342e960c0ea4dfe495bc2a
kizahasi <rasis.aneki@gmail.com>
2022-07-30 19:40:57 +0900

    fix: ensure properties not found to be undefined

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 export type Ok<TValue> = {
     isError: false;
     value: TValue;
-    error?: undefined;
+    error: undefined;
 };
 
 export type Error<TError = string> = {
     isError: true;
-    value?: undefined;
+    value: undefined;
     error: TError;
 };
 
 export type Result<TValue, TError = string> = Ok<TValue> | Error<TError>;
 
 export const Result = {
-    ok: <TValue>(value: TValue): Ok<TValue> => ({ isError: false, value }),
-    error: <TError>(error: TError): Error<TError> => ({ isError: true, error }),
+    ok: <TValue>(value: TValue): Ok<TValue> => ({ isError: false, value, error: undefined }),
+    error: <TError>(error: TError): Error<TError> => ({ isError: true, value: undefined, error }),
     get: <TValue>(source: Result<TValue>): TValue => {
         if (source.isError) {
             throw new Error('isError must not be true');
